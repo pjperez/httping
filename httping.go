8a131495d2bd9f932f4f17984d0b48f641be0229
Pedro Perez <pjperez@gmail.com>
2016-09-22 17:58:07 +0100

    Version 0.5

--- httping.go
+++ httping.go
@@ -1,4 +1,4 @@
-// httping 0.2 - A tool to measure RTT on HTTP/S requests
+// httping 0.5 - A tool to measure RTT on HTTP/S requests
 // This software is distributed AS IS and has no warranty. This is merely a learning exercise and should not be used in production under any circumstances.
 // This is my own work and not that of my employer, not is endorsed or supported by them in any conceivable way.
 // Pedro Perez - pjperez@outlook.com
@@ -16,20 +16,25 @@ import (
 	"os/signal"
 	"strings"
 	"time"
+
+	"github.com/montanaflynn/stats"
 )
 
 func main() {
 
 	httpverbPtr := flag.String("httpverb", "GET", "HTTP Verb: GET or HEAD")
 	countPtr := flag.Int("count", 10, "Number of requests to send")
+	httpVerb := *httpverbPtr
 
 	flag.Parse()
 
-	fmt.Println("\nhttping 0.2 - A tool to measure RTT on HTTP/S requests")
+	fmt.Println("\nhttping 0.5 - A tool to measure RTT on HTTP/S requests")
 	fmt.Println("Help: httping -h")
 
 	urlStr := flag.Args()[0]
 
+	// Check what protocol has been specified in the URL by checking the first 7 or 8 chars.
+	// If none specified, fall back to HTTP
 	if len(flag.Args()[0]) > 6 {
 		if urlStr[:7] != "http://" {
 			if urlStr[:8] != "https://" {
@@ -46,6 +51,7 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Parse URL and fail if the host can't be resolved.
 	url, err := url.Parse(urlStr)
 	if err != nil {
 		fmt.Println("Cannot resolve: " + urlStr)
@@ -53,29 +59,34 @@ func main() {
 		return
 	}
 
-	httpVerb := *httpverbPtr
-
 	fmt.Printf("HTTP %s to %s (%s):\n", httpVerb, url.Host, urlStr)
 	ping(httpVerb, url, *countPtr)
 }
 
 func ping(httpVerb string, url *url.URL, count int) {
-	// This function loops indefinitely (TODO- Select number of iterations) and prints result on screen after each loop
+	// This function is responsible to send the requests, count the time and show statistics when finished
 
+	// Initialise needed variables
 	timeTotal := time.Duration(0)
-	timeout := time.Duration(2 * time.Second)
 	i := 1
+	successfulProbes := 0
+	var responseTimes []float64
+
+	// Change request timeout to 2 seconds
+	timeout := time.Duration(2 * time.Second)
 	client := http.Client{
 		Timeout: timeout,
 	}
-	successfulProbes := 0
+
 	result, err := client.Get(url.String())
 
+	// Send requests for url, "count" times
 	for i = 1; count >= i; i++ {
-
+		// Initialise variables
 		timeStart := time.Now()
 		responseTime := time.Since(timeStart)
-		// Send GET or HEAD request
+
+		// Send GET or HEAD request, depending on httpVerb received
 		if httpVerb == "GET" {
 			timeStart = time.Now()
 			result, err = client.Get(url.String())
@@ -90,6 +101,7 @@ func ping(httpVerb string, url *url.URL, count int) {
 			fmt.Println("Fatal error!", err)
 		}
 
+		// Add all the response times to calculate the average later
 		timeTotal += responseTime
 
 		// Calculate the downloaded bytes
@@ -99,9 +111,10 @@ func ping(httpVerb string, url *url.URL, count int) {
 		// Print result on screen
 		fmt.Printf("connected to %s, seq=%d, httpVerb=%s, httpStatus=%d, bytes=%d, RTT=%.2f ms\n", url, i, httpVerb, result.StatusCode, bytes, float32(responseTime)/1e6)
 
-		// Count how many probes are successful, i.e. how many get a 200 HTTP StatusCode
+		// Count how many probes are successful, i.e. how many get a 200 HTTP StatusCode - If successful also add the result to a slice "responseTimes"
 		if result.StatusCode == 200 {
 			successfulProbes++
+			responseTimes = append(responseTimes, float64(responseTime))
 		}
 
 		time.Sleep(1e9)
@@ -118,7 +131,36 @@ func ping(httpVerb string, url *url.URL, count int) {
 		}()
 	}
 
+	// Let's calculate and spill some results
+	// 1. Average response time
 	timeAverage := time.Duration(int64(timeTotal) / int64(successfulProbes))
 
-	fmt.Println("\nProbes sent:", i-1, "\nSuccessful responses:", successfulProbes, "\nAverage response time:", timeAverage)
+	// 2. Min and Max response times
+	var biggest float64
+	smallest := float64(1000000000)
+
+	for _, v := range responseTimes {
+
+		if v > biggest {
+			biggest = v
+		}
+
+		if v < smallest {
+			smallest = v
+		}
+
+	}
+
+	// 3. Median response time
+	median, _ := stats.Median(responseTimes)
+
+	// 4. Percentile
+	percentile90, _ := stats.Percentile(responseTimes, float64(90))
+	percentile75, _ := stats.Percentile(responseTimes, float64(75))
+	percentile50, _ := stats.Percentile(responseTimes, float64(50))
+	percentile25, _ := stats.Percentile(responseTimes, float64(25))
+
+	fmt.Println("\nProbes sent:", i-1, "\nSuccessful responses:", successfulProbes, "\n% of requests failed:", float64(100-(successfulProbes*100)/(i-1)), "\nMin response time:", time.Duration(smallest), "\nAverage response time:", timeAverage, "\nMedian response time:", time.Duration(median), "\nMax response time:", time.Duration(biggest))
+
+	fmt.Println("\n90% of requests were faster than:", time.Duration(percentile90), "\n75% of requests were faster than:", time.Duration(percentile75), "\n50% of requests were faster than:", time.Duration(percentile50), "\n25% of requests were faster than:", time.Duration(percentile25))
 }
